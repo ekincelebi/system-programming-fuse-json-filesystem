5524d4b9b5e9475ea7e524e49019082b5eececed
geceavsar <geceavsar@gmail.com>
2021-01-19 01:25:24 +0200

    file add done

--- filesystem.c
+++ filesystem.c
@@ -41,7 +41,6 @@ static cJSON* cJSON_by_name(cJSON *parent, const char *name)
 {
     cJSON * ptr = parent;
     while (ptr) {
-		printf("%s %s\n", ptr->string, name);
         if (strcmp(ptr->string, name) == 0){
 			return ptr;
 		}
@@ -70,6 +69,60 @@ static cJSON * cJSON_by_path(cJSON * parent, const char * path){
 	}
 }
 
+static char * cJSON_parent(const char * path){
+	
+	if(strcmp(path, "/") == 0){
+		return "/";
+	}
+
+	char * parent_path = NULL;
+	int i = strlen(path) -1;
+
+	for(; path[i] && i>=0; i--){
+			if (path[i] == '/'){
+				if(i==0){ //parent is root
+					parent_path = calloc(i+1, sizeof(char));
+					strcpy(parent_path, "/");
+				}
+				else{
+					parent_path = calloc(i, sizeof(char));
+					memcpy(parent_path, path, i);
+				}
+				break;
+			}
+		}
+
+	return parent_path; 
+}
+
+static char * cJSON_basename(const char * path){
+	char * basename, * parent_path;
+	parent_path = cJSON_parent(path);
+	if(strcmp(parent_path, "/")!=0){
+		int parent_len = strlen(parent_path); 
+		int path_len = strlen(path); 
+		int diff = path_len - parent_len; 
+		basename = calloc(diff, sizeof(char));
+		memcpy(basename, path + parent_len +1, diff-1);
+	}
+	if(strcmp(parent_path, "/")==0){
+		int parent_len = strlen(parent_path); 
+		int path_len = strlen(path); 
+		int diff = path_len - parent_len; 
+		basename = calloc(diff, sizeof(char));
+		memcpy(basename, path + parent_len, diff);
+	}
+	return basename;
+}
+
+static void cJSON_edit_source(){
+	FILE * f = fopen(filename, "w");
+	char * newJsonText = cJSON_Print(root);
+	printf("%s\n", newJsonText);
+	if(newJsonText) fprintf(f, "%s", newJsonText);
+	else fprintf(stderr, "Cannot write to the file!");
+	fclose(f);
+}
 
 ///// json methods
 
@@ -162,35 +215,6 @@ static int do_read( const char *path, char *buffer, size_t size, off_t offset, s
     return size;
 }
 
-static char * cJSON_parent(const char * path){
-	
-	if(strcmp(path, "/") == 0){
-		return "/";
-	}
-
-	cJSON * ptr = cJSON_by_path(root, path);
-	if(ptr == NULL) return NULL;
-
-
-	char * parent_path = NULL;
-	int i = strlen(path) -1;
-
-	for(; path[i] && i>=0; i--){
-			if (path[i] == '/'){
-				if(i==0){ //parent is root
-					parent_path = calloc(i+1, sizeof(char));
-					strcpy(parent_path, "/");
-				}
-				else{
-					parent_path = calloc(i, sizeof(char));
-					memcpy(parent_path, path, i);
-				}
-				break;
-			}
-		}
-
-	return parent_path; 
-}
 
 static int do_unlink( const char *path, struct fuse_file_info *fi ){
 	
@@ -215,7 +239,53 @@ static int do_unlink( const char *path, struct fuse_file_info *fi ){
 }
 
 static int do_open(const char * path, struct fuse_file_info *fi){
-	return 0;
+    return 0;
+}
+
+static int do_create(const char * path, struct fuse_file_info *fi){
+	return do_open(path, fi);	
+}
+
+static int do_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+	
+	cJSON * ptr = cJSON_by_path(root, path);
+	
+	if(ptr!=NULL) return 0;
+
+    char *parent_path = cJSON_parent(path);
+    
+	char *basename = cJSON_basename(path);
+	
+	if(basename == NULL) return 0;
+    
+	cJSON * parent_json = cJSON_by_path(root, parent_path);
+	cJSON * new_json = cJSON_CreateObject();
+    
+	cJSON_AddStringToObject(parent_json, basename, "");
+	
+	ptr->valuestring = realloc(ptr->valuestring, (offset+size+1) * sizeof(char));
+
+    memcpy(ptr->valuestring + offset, buf, size);
+
+	ptr->valuestring[size] = '\0';
+	
+	cJSON_edit_source();
+
+	return offset;
+}
+
+static int do_truncate(const char *path, off_t size)
+{
+    (void)path;
+    (void)size;
+    return 0;
+}
+
+static int do_release(const char *path, struct fuse_file_info *finfo)
+{
+    (void) path;
+    (void) finfo;
+    return 0;
 }
 
 static struct fuse_operations operations = {
@@ -224,8 +294,10 @@ static struct fuse_operations operations = {
     .read		= do_read, //cat
 	.unlink		= do_unlink,
 	.open		= do_open,
-	//.mkdir		= do_mkdir,
-	//.mknod		= do_mknod,
+	.create		= do_create,
+	.write		= do_write,
+	.truncate	= do_truncate,
+	.release	= do_release,
 };
 
 int main( int argc, char *argv[] )
